73b2c0c6842f672321e531a311a545b4dfc7a8c5
lucas.burlingham@icloud.com <lucas.burlingham@icloud.com>
2020-11-24 19:55:46 -0500

    stupid me forgot type="submit" on submit button :(

--- requests.php
+++ requests.php
@@ -16,11 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <title>requests.php</title>
 </head>
 
@@ -31,9 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <p class="lead">You are viewing this page because at least some of the PHP behind it works, and has no serious errors.</p>
         </div>
     </div>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
