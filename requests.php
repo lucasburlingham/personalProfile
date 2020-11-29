d6427e3a95ba83ff5a0ae5500677df7d5b13e4f8
lucas.burlingham@icloud.com <lucas.burlingham@icloud.com>
2020-11-28 20:40:00 -0500

    secured and added error messages

--- requests.php
+++ requests.php
@@ -1,13 +1,13 @@
 <?php
 
-if ($_POST['REQUEST_METHOD'] == 'POST') {
+if (!(empty($_REQUEST["name"])) || !(empty($_REQUEST["email"])) || !(empty($_REQUEST['message']))) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = "From" . $name . $email . "Personal Page Form";
     $message = $_POST['message'];
     mail("lucas.burlingham@icloud.com", $subject, $message);
 } else {
-    echo "An error has occured.";
+    error();
 }
 ?>
 <!DOCTYPE html>
@@ -26,6 +26,20 @@ if ($_POST['REQUEST_METHOD'] == 'POST') {
         <div class="container">
             <h1 class="display-3">At least it works</h1>
             <p class="lead">You are viewing this page because at least some of the PHP behind it works, and has no serious errors.</p>
+        </div>
+        <div>
+            <p>Status: <?php echo "An error has occured.";
+                        echo "<h3>Here are the details that the server received:</h3>";
+                        $email = $_POST["email"];
+                        $name = $_POST["name"];
+                        $message = $_REQUEST["message"];
+                        echo "<b>";
+                        echo "Email: " . $email;
+                        echo "<b>";
+                        echo 'Message: ' . $message;
+                        echo "<b>";
+                        echo 'Name: ' . $name; ?>
+            </p>
         </div>
     </div>
 </body>
