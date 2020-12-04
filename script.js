62f865ea274abd725bd5f41a01f4c41d8af04fcd
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-12-03 21:08:54 -0500

    Created clipboard + popup function

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ new TypeIt("#typeTitle", {
 function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+function clip() {
+    var copyText = document.getElementById("discordClip");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    console.log("Copied the text. Displaying alert now...");
+    document.getElementById("discordAlert").style.display = "inherit";
+}
