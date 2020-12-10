e6fc6a09246eed4f6c6b149ee46790e101959fd6
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-12-10 16:28:50 -0500

    Added recent output error, confirmation sent

--- requests.php
+++ requests.php
@@ -8,6 +8,16 @@ if (!(empty($_REQUEST["name"])) || !(empty($_REQUEST["email"])) || !(empty($_REQ
     $messageBody = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
     $message = "The sender of this message " . $name . " has written: \r\n \r\n" .  "   " . $messageBody;
     mail("lucas.burlingham@icloud.com", $subject, $message);
+    $confirmationMessage = "Thank you, $name, for sending a message through https://lucasburlingham.me. Your message has been received, and has been forwarded to Lucas Burlingham. \r\n\n";
+    $confirmationSubject = "From lucasburlingham.me: Thank you, $name, ";
+    mail($email, $confirmationSubject, $confirmationMessage);
+    $recentOutput = fopen("recentOutput.txt", "a") or die("Unable to open file!");
+    fwrite($recentOutput, $name);
+    fwrite($recentOutput, "\n");
+    fwrite($recentOutput, $email);
+    fwrite($recentOutput, "\n");
+    fwrite($recentOutput, $message);
+    fwrite($recentOutput, "\n");
 } else {
     error();
 }
@@ -30,21 +40,29 @@ if (!(empty($_REQUEST["name"])) || !(empty($_REQUEST["email"])) || !(empty($_REQ
             <p class="lead">You are viewing this page because at least some of the PHP behind it works, and has no serious errors.</p>
         </div>
         <div>
+            <p>If there are errors, they will be listed below:</p>
+        </div>
+        <div>
             <p>Status: <?php
                         function error()
                         {
-                            echo "An error has occurred.";
-                            echo "<h3>Here are the details that the server received:</h3>";
-                            $email = $_POST["email"];
-                            $name = $_POST["name"];
-                            $message = $_REQUEST["message"];
-                            echo "<b>";
-                            echo "Email: " . $email;
-                            echo "<b>";
-                            echo 'Message: ' . $message;
-                            echo "<b>";
-                            echo 'Name: ' . $name;
+                            echo "An error has occurred.<br>";
+                            echo "<h3>Here are the last details that the server received:</h3>";
+                            if (recentFileCheck() == FALSE) {
+                                $recentOutput = fopen("recentOutput.txt", "a");
+                            }
+                            $recentOutput = "recentOutput.txt";
+                            $lines = file($recentOutput);
+                            echo "Email: " . $lines[0] . "<br>";
+                            echo "Message: " . $lines[1] . "<br>";
+                            echo "Name: " . $lines[2 . "<br>"];
                         }
+
+                        function recentFileCheck()
+                        {
+                            return file_exists("recentOutput.txt");
+                        }
+                        fclose($recentOutput);
                         ?>
             </p>
         </div>
