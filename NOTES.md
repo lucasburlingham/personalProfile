cc630fce1c2e460f0e298cdb876ba19afd6af1f9
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-04-28 21:59:58 -0400

    Remove projects in navbar

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,5 @@
+googleb79068ce4e19975e.html
+googleb79068ce4e19975e.html
 ## Color Palette
 
 https://coolors.co/ff5c5c-99a1a6-343a40-161f27-bfbdc1
--- script.js
+++ script.js
@@ -1,69 +1,14 @@
-document.getElementById("submitButton").onclick = addElement;
-
-function addElement() {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Form Submitted");
-    confetti();
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById("div");
-    document.body.insertBefore(newDiv, currentDiv);
-}
-
-// $("#contactForm").ajaxForm({
-//     url: "https://cloud.golca.org/requests.php",
-//     type: 'post'
-// });
-
-
-new TypeIt("#nameType", {
-    speed: 70,
-    loop: false,
-    waitUntilVisible: true,
-    afterComplete: function (step, instance) {
-        instance.destroy();
-    }
-}).go();
-
-
-new TypeIt("#aboutType", {
-    lifeLike: false,
-    startDelay: 1500,
-    speed: 50,
-    cursor: false,
-    loop: false
-}).go();
-new TypeIt("#messageType", {
-    lifeLike: true,
-    startDelay: 2000,
-    speed: 90,
-    loop: false
-}).go();
-new TypeIt("#typeTitle", {
-    lifeLike: true,
-    speed: 90,
-    loop: false
-})
-
 function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 new ClipboardJS('.btn');
 
-// function clip() {
-//     var copyText = document.getElementById("discordClip");
-//     copyText.select();
-//     copyText.setSelectionRange(0, 99999);
-//     document.execCommand("copy");
-//     console.log("Copied the text. Displaying alert now...");
-//     document.getElementById("discordAlert").style.display = "inherit";
-// }
-
-// $('[data-toggle="popover"]').popover({
-//     placement: 'top',
-//     trigger: 'hover'
-// });
-
-function onSubmit(token) {
-    document.getElementById("contactForm").submit();
+function clip() {
+    var copyText = document.getElementById("discordClip");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    console.log("Copied the text. Displaying alert now...");
+    document.getElementById("discordAlert").style.display = "inherit";
 }
